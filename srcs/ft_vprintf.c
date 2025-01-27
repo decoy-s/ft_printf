3c752ff4ba91fcc1d951211e5ab5834fb8e52e3c
Sakura <dec0y@duck.com>
2025-01-28 02:50:54 +0900

    Update ft_vprintf.c

--- srcs/ft_vprintf.c
+++ srcs/ft_vprintf.c
@@ -15,10 +15,12 @@ int ft_vprintf(const char *format, va_list args)
 			if(format[i] == 'c')
 				len += ft_putchar(va_arg(args, int));
 			//va_argで引数を取得し、putcharする
-			else if(format[i] =='s')
+			else if(format[i] == 's')
 				len += ft_putstr(va_arg(args, char *));
 			else if(format[i] == 'd' || format[i] == 'i')
 				len += ft_putnbr(va_arg(args, int));
+			else if (format[i] == "%")
+				len += i + 1;
 		}
 		else
 			len += ft_putchar(format[i]);
